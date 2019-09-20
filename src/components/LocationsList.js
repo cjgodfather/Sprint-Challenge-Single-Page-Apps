922cffee29f32554a4a8609ab9fd698f36c54d8c
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-09-20 13:39:25 -0400

    challenge complete

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,38 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
-export default function LocationsList() {}
+import LocationCard from "./LocationCard";
+
+const WrappedDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export default function LocationsList() {
+  const [locationList, setLocationList] = useState([]);
+
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(`https://rickandmortyapi.com/api/location/`)
+      .then(res => setLocationList(res.data.results));
+  }, []);
+
+  return (
+    <WrappedDiv>
+      {locationList
+        ? locationList.map(location => (
+            <LocationCard
+              key={location.name}
+              name={location.name}
+              type={location.type}
+              dimension={location.dimension}
+            />
+          ))
+        : null}
+    </WrappedDiv>
+  );
+}
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
 
-export default function SearchForm() {
- 
+const FormContainer = styled.section`
+  width: 100%;
+  text-align: center;
+`;
+
+const StyledInput = styled.input`
+  border: 1px black solid;
+  border-radius: 2px;
+  margin-left: 5px;
+`;
+
+export default function SearchForm(props) {
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <FormContainer>
+      <form>
+        <label>Search Character by Name:</label>
+        <StyledInput
+          id="name"
+          type="text"
+          name="textfield"
+          value={props.searchTerm}
+          onChange={props.handleChange}
+        />
+      </form>
+    </FormContainer>
   );
 }
